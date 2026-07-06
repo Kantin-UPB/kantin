e94d04c33acc898850eecc1d2e9ebbd86d610b2d
LV <elva.gracia2810@gmail.com>
2026-07-06 15:36:20 +0700

    CRUD  Menu

---
+++ app/Config/Status.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Config;
+
+class Status
+{
+    public const DATA = [
+        1  => 'Pending',
+        5  => 'Active',
+        8  => 'Cancel',
+        20 => 'Terposting',
+    ];
+}
